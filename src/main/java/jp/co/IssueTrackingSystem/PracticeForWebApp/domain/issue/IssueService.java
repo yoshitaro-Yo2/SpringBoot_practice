6a683e11edb51350ff9a4f1a71431e46e1870bb8
yoshitaro-Yo2 <kichivs.mp@gmail.com>
2022-11-01 18:53:26 +0900

    ServiceクラスをDIコンテナに登録、Controllerにてインジェクションし処理を委譲する。

--- src/main/java/jp/co/IssueTrackingSystem/PracticeForWebApp/domain/issue/IssueService.java
+++ src/main/java/jp/co/IssueTrackingSystem/PracticeForWebApp/domain/issue/IssueService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class IssueService {
     public List<IssueEntity> findAllIssues() {
         var issueList = List.of(
--- src/main/java/jp/co/IssueTrackingSystem/PracticeForWebApp/issue/IssueController.java
+++ src/main/java/jp/co/IssueTrackingSystem/PracticeForWebApp/issue/IssueController.java
@@ -1,14 +1,16 @@
 package jp.co.IssueTrackingSystem.PracticeForWebApp.issue;
 
 import jp.co.IssueTrackingSystem.PracticeForWebApp.domain.issue.IssueService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class IssueController {
 
-    private final IssueService issueService = new IssueService();
+    private final IssueService issueService;
 
     @GetMapping("/issues")
     public String showList(Model model){
