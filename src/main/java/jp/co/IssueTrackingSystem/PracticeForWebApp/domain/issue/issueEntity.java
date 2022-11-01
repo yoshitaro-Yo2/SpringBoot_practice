7eae6296e877d6ec9980be1bb2bdc918f10914bd
yoshitaro-Yo2 <kichivs.mp@gmail.com>
2022-11-01 18:14:52 +0900

    Lombokの導入に伴うEntityのリファクタ

--- src/main/java/jp/co/IssueTrackingSystem/PracticeForWebApp/domain/issue/issueEntity.java
+++ src/main/java/jp/co/IssueTrackingSystem/PracticeForWebApp/domain/issue/issueEntity.java
@@ -1,37 +1,12 @@
 package jp.co.IssueTrackingSystem.PracticeForWebApp.domain.issue;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class issueEntity {
     private long id;
     private String summary;
     private String description;
-
-    public issueEntity(long id, String summary, String description) {
-        this.id = id;
-        this.summary = summary;
-        this.description = description;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getSummary() {
-        return summary;
-    }
-
-    public void setSummary(String summary) {
-        this.summary = summary;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
