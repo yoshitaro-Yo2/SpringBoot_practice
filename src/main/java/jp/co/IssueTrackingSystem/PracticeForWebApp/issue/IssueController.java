dca6e129084dbcdf5598b90cd9ebc29da6e79eb5
yoshitaro-Yo2 <kichivs.mp@gmail.com>
2022-11-02 11:40:15 +0900

    課題作成画面・入力フォーム・TOPページリンクの作成

--- src/main/java/jp/co/IssueTrackingSystem/PracticeForWebApp/issue/IssueController.java
+++ src/main/java/jp/co/IssueTrackingSystem/PracticeForWebApp/issue/IssueController.java
@@ -5,17 +5,24 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/issues")
 @RequiredArgsConstructor
 public class IssueController {
 
     private final IssueService issueService;
 
-    @GetMapping("/issues")
+    @GetMapping
     public String showList(Model model){
 
         model.addAttribute("issueList", issueService.findAllIssues());
         return "issues/list";
+    }
+
+    @GetMapping("/creationForm")
+    public String showCreationForm(){
+        return "issues/creationForm";
     }
 }
