2eebd284e193d16148aadeced41e4a94efe610f4
yoshitaro-Yo2 <kichivs.mp@gmail.com>
2022-11-02 14:11:39 +0900

    バリデーション機能とエラーメッセージ表示

--- src/main/java/jp/co/IssueTrackingSystem/PracticeForWebApp/issue/IssueController.java
+++ src/main/java/jp/co/IssueTrackingSystem/PracticeForWebApp/issue/IssueController.java
@@ -4,6 +4,8 @@ import jp.co.IssueTrackingSystem.PracticeForWebApp.domain.issue.IssueService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,7 +32,10 @@ public class IssueController {
     }
 
     @PostMapping
-    public String Create(IssueForm form, Model model){
+    public String Create(@Validated IssueForm form, BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()) {
+            return showCreationForm(form);
+        }
         issueService.create(form.getSummary(), form.getDescription());
         return "redirect:/issues";
     }
