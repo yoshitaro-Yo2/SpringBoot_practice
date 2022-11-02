f16b28b79e02aa3a6d2997b8605a5a6f47fca3b4
yoshitaro-Yo2 <kichivs.mp@gmail.com>
2022-11-02 10:45:25 +0900

    テーブル作成と初期データ投入

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT into issues(summary, description) values ('バグA', 'バグがあります');
+INSERT into issues(summary, description) values ('機能要望B', 'Bに追加機能がほしいです');
+INSERT into issues(summary, description) values ('画面Cが遅い', '早くしてほしいです');
